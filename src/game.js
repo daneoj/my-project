18a56eac2555ace747be1e9c646dc6978bf8d3cc
Dane <dane.ojala@gmail.com>
2023-05-21 16:21:31 -0700

    init commit

---
+++ src/game.js
@@ -0,0 +1,30 @@
+var canvas = document.getElementById("canvas")
+var width = canvas.width
+var height = canvas.height
+var ctx = canvas.getContext("2d")
+ctx.fillStyle = "red"
+
+var dobby = {
+    x: width / 2
+    y: height / 2
+}
+
+function update(dt) {
+
+}
+
+function draw() {
+    ctx.
+}
+
+function loop(time) {
+    var dt = time - lastTime
+    update(dt)
+    draw()
+
+    lastRender = time
+    window.requestAnimationFrame(loop)
+}
+
+var lastRender = 0
+window.requestAnimationFrame(loop)
